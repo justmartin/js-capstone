901b640e03cc7a1ab9a8d1f46731655ffe64ccf2
justmartin <justin.lamar.martin@gmail.com>
2016-09-29 10:30:58 -0700

    disabled letter button after user selects it once

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -4,120 +4,140 @@ $(document).ready(function() {
 		a = $("#a")[0].value;
 		storeGuessedLetters(a);
 		guessClickedLetter(a);
+		$("#a").attr("disabled", "true");
 	});
 
 	$("#b").click(function() {
 		b = $("#b")[0].value;
 		storeGuessedLetters(b);
 		guessClickedLetter(b);
+		$("#b").attr("disabled", "true");
 	});
 	
 	$("#c").click(function() {
 		c = $("#c")[0].value;
 		storeGuessedLetters(c);
 		guessClickedLetter(c);
+		$("#c").attr("disabled", "true");
 	});
 
 	$("#d").click(function() {
 		d = $("#d")[0].value;
 		storeGuessedLetters(d);
 		guessClickedLetter(d);
+		$("#d").attr("disabled", "true");
 	});
 
 	$("#e").click(function() {
 		e = $("#e")[0].value;
 		storeGuessedLetters(e);
 		guessClickedLetter(e);
+		$("#e").attr("disabled", "true");
 	});
 
 	$("#f").click(function() {
 		f = $("#f")[0].value;
 		storeGuessedLetters(f);
 		guessClickedLetter(f);
+		$("#f").attr("disabled", "true");
 	});
 	
 	$("#g").click(function() {
 		g = $("#g")[0].value;
 		storeGuessedLetters(g);
 		guessClickedLetter(g);
+		$("#g").attr("disabled", "true");
 	});
 
 	$("#h").click(function() {
 		h = $("#h")[0].value;
 		storeGuessedLetters(h);
 		guessClickedLetter(h);
+		$("#h").attr("disabled", "true");
 	});
 
 	$("#i").click(function() {
 		i = $("#i")[0].value;
 		storeGuessedLetters(i);
 		guessClickedLetter(i);
+		$("#i").attr("disabled", "true");
 	});
 
 	$("#j").click(function() {
 		j = $("#j")[0].value;
 		storeGuessedLetters(j);
 		guessClickedLetter(j);
+		$("#j").attr("disabled", "true");
 	});
 	
 	$("#k").click(function() {
 		k = $("#k")[0].value;
 		storeGuessedLetters(k);
 		guessClickedLetter(k);
+		$("#k").attr("disabled", "true");
 	});
 
 	$("#l").click(function() {
 		l = $("#l")[0].value;
 		storeGuessedLetters(l);
 		guessClickedLetter(l);
+		$("#l").attr("disabled", "true");
 	});
 
 	$("#m").click(function() {
 		m = $("#m")[0].value;
 		storeGuessedLetters(m);
 		guessClickedLetter(m);
+		$("#m").attr("disabled", "true");
 	});
 
 	$("#n").click(function() {
 		n = $("#n")[0].value;
 		storeGuessedLetters(n);
 		guessClickedLetter(n);
+		$("#n").attr("disabled", "true");
 	});
 	
 	$("#o").click(function() {
 		o = $("#o")[0].value;
 		storeGuessedLetters(o);
 		guessClickedLetter(o);
+		$("#o").attr("disabled", "true");
 	});
 
 	$("#p").click(function() {
 		p = $("#p")[0].value;
 		storeGuessedLetters(p);
 		guessClickedLetter(p);
+		$("#p").attr("disabled", "true");
 	});
 
 	$("#q").click(function() {
 		q = $("#q")[0].value;
 		storeGuessedLetters(q);
 		guessClickedLetter(q);
+		$("#q").attr("disabled", "true");
 	});
 
 	$("#r").click(function() {
 		r = $("#r")[0].value;
 		storeGuessedLetters(r);
 		guessClickedLetter(r);
+		$("#r").attr("disabled", "true");
 	});
 	
 	$("#s").click(function() {
 		s = $("#s")[0].value;
 		storeGuessedLetters(s);
 		guessClickedLetter(s);
+		$("#s").attr("disabled", "true");
 	});
 
 	$("#t").click(function() {
 		t = $("#t")[0].value;
 		storeGuessedLetters(t);
 		guessClickedLetter(t);
+		$("#t").attr("disabled", "true");
 	});
 
 
@@ -125,36 +145,42 @@ $(document).ready(function() {
 		u = $("#u")[0].value;
 		storeGuessedLetters(u);
 		guessClickedLetter(u);
+		$("#u").attr("disabled", "true");
 	});
 
 	$("#v").click(function() {
 		v = $("#v")[0].value;
 		storeGuessedLetters(v);
 		guessClickedLetter(v);
+		$("#v").attr("disabled", "true");
 	});
 
 	$("#w").click(function() {
 		w = $("#w")[0].value;
 		storeGuessedLetters(w);
 		guessClickedLetter(w);
+		$("#w").attr("disabled", "true");
 	});
 
 	$("#x").click(function() {
 		x = $("#x")[0].value;
 		storeGuessedLetters(x);
 		guessClickedLetter(x);
+		$("#x").attr("disabled", "true");
 	});
 
 	$("#y").click(function() {
 		y = $("#y")[0].value;
 		storeGuessedLetters(y);
 		guessClickedLetter(y);
+		$("#y").attr("disabled", "true");
 	});
 
 	$("#z").click(function() {
 		z = $("#z")[0].value;
 		storeGuessedLetters(z);
 		guessClickedLetter(z);
+		$("#z").attr("disabled", "true");
 	});
 		
 });
